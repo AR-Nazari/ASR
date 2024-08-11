c4d4fa151c027387e72ef791933e81aa38d6cc42
Alireza <alirezafazli29@gmail.com>
2024-08-11 11:11:28 +0330

    add GNet_MLP class

--- GNet.py
+++ GNet.py
@@ -1,1 +1,31 @@
 import torch 
+import torch.nn as nn
+
+
+class GNet_MLP(nn.Module):
+
+    def __init__(self):
+        super(GNet_MLP, self).__init__()
+        self.fc1 = nn.Linear(in_features=988, out_features=512)
+        self.fc2 = nn.Linear(in_features=512, out_features=256)
+        self.fc3 = nn.Linear(in_features=256, out_features=128)
+        self.fc4 = nn.Linear(in_features=128, out_features=64)
+        self.fc5 = nn.Linear(in_features=64, out_features=32)
+        self.fc6 = nn.Linear(in_features=32, out_features=2)
+        self.softmax = nn.Softmax(dim=-1) # For binary classification
+        self.relu = nn.ReLU()
+
+    def forward(self, x):
+        x = self.fc1(x)
+        x = self.relu(x)
+        x = self.fc2(x)
+        x = self.relu(x)
+        x = self.fc3(x)
+        x = self.relu(x)
+        x = self.fc4(x)
+        x = self.relu(x)
+        x = self.fc5(x)
+        x = self.relu(x)
+        x = self.fc6(x)
+        x = self.softmax(x)
+        return x
