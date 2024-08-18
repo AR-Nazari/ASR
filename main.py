224c65d116819f20cd8b96ad85f9870c3e0daa8f
Alireza <alirezafazli29@gmail.com>
2024-08-18 08:34:57 +0330

    define Load_n_process_audio to read audio files

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
+import numpy as np
 import GNet
 import MyAudio
 from transformers import AutoModelForSpeechSeq2Seq, AutoProcessor
 
+
 # Load Models
 Gender_Classification_MLP = GNet.ModelLoader(GNet.ModelType.best_epoch)
 ASR_Whisper = AutoModelForSpeechSeq2Seq.from_pretrained(pretrained_model_name_or_path="openai/whisper-large-v3")
+
 
 # Load Procesoors
 GNet_Processor = GNet.WaveFormProcess()
 Whisper_Processor = AutoProcessor.from_pretrained("openai/whisper-large-v3")
 
 
+def Load_n_process_audio(file_path, separator_output_directory):
+    Audio_chunks = MyAudio.pipe(file_path, separator_output_directory)
+    Waveforms = [np.array(chunk.normalize().set_channels(1).get_array_of_samples()) for chunk in Audio_chunks]
+    return Waveforms
