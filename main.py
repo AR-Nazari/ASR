3c92118cd97f99a617cecc42fc4bbfc1afa84726
Alireza <alirezafazli29@gmail.com>
2024-08-18 11:36:58 +0330

    work with comments

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ Gender_Classification_MLP = GNet.ModelLoader(GNet.ModelType.best_epoch)
 ASR_Whisper = AutoModelForSpeechSeq2Seq.from_pretrained(pretrained_model_name_or_path="openai/whisper-large-v3")
 
 
-# Load Procesoors
+# Load Processors
 GNet_Processor = GNet.WaveFormProcess()
 Whisper_Processor = AutoProcessor.from_pretrained("openai/whisper-large-v3")
 
