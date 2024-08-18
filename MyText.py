81b2afdaf20d6c91ed589ab80b0d84a572b146ff
Alireza <alirezafazli29@gmail.com>
2024-08-18 10:15:00 +0330

    update text_pipe in MyText so it can ignore numbers in spell correction

--- MyText.py
+++ MyText.py
@@ -1,4 +1,5 @@
 import string
+import re
 import hazm
 from parsivar import SpellCheck
 
@@ -8,7 +9,20 @@ normalizer = hazm.Normalizer()
 punc = string.punctuation + '،' + '؛'
 
 def text_pipe(input_sentence):
+
+    # remove punctuation
     text = ''.join([char for char in input_sentence if char not in punc])
+
+    # ignore numbers and correct misspells
+    numbers = re.findall(r'\d+', text)
+    placeholders = [f"NUMBER{i}" for i in range(len(numbers))]
+    for number, placeholder in zip(numbers, placeholders):
+        text = text.replace(number, placeholder)
     text = spell_checker.spell_corrector(text)
+    for number, placeholder in zip(numbers, placeholders):
+        text = text.replace(placeholder, number)
+
+    # normalize
     text = normalizer.normalize(text)
+    
     return text
