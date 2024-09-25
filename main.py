ade1bd5f23306928e4ad3fd512ab7f4cfaae6053
Ali Reza Nazari <Ali.Reza.Nazari@gmail.com>
2024-09-25 09:41:20 +0330

    working on api

--- main.py
+++ main.py
@@ -4,18 +4,36 @@ import GNet
 import MyAudio
 import MyText
 from transformers import AutoModelForSpeechSeq2Seq, AutoProcessor
+from fastapi import FastAPI, UploadFile, File
+from fastapi.responses import HTMLResponse
+from pydantic import BaseModel
+import shutil
+import os
+import uvicorn
 
-default_device = 'cuda' if torch.cuda.is_available() else 'cpu'
-print(f'Selected device for calculations is {torch.cuda.get_device_name()}') if torch.cuda.is_available() else print(f'Selected device for calculations is cpu')
-
-# Load Models
-Gender_Classification_MLP = GNet.ModelLoader(GNet.ModelType.best_epoch, device=default_device)
-ASR_Whisper = AutoModelForSpeechSeq2Seq.from_pretrained(pretrained_model_name_or_path="openai/whisper-large-v3").to(default_device)
 
 
-# Load Processors
-GNet_Processor = GNet.WaveFormProcess()
-Whisper_Processor = AutoProcessor.from_pretrained("openai/whisper-large-v3")
+#------------------------------------------------------------------------------------------------------------------------------------------------------------------#
+def select_device():
+    print(f'Selected device for calculations is {torch.cuda.get_device_name()}') if torch.cuda.is_available() else print(f'Selected device for calculations is cpu')
+    return 'cuda' if torch.cuda.is_available() else 'cpu'
+#------------------------------------------------------------------------------------------------------------------------------------------------------------------#
+
+
+# default_device = select_device()
+
+# # Load Models
+# Gender_Classification_MLP = GNet.ModelLoader(GNet.ModelType.best_epoch, device=default_device)
+# ASR_Whisper = AutoModelForSpeechSeq2Seq.from_pretrained(pretrained_model_name_or_path="openai/whisper-large-v3", torch_dtype=torch.float16).to(default_device)
+
+# print('Models Loaded')
+
+# # Load Processors
+# GNet_Processor = GNet.WaveFormProcess()
+# Whisper_Processor = AutoProcessor.from_pretrained("openai/whisper-large-v3")
+
+# print('Processors Loaded')
+
 
 #--------------------------------------------------------------------------------------------------------------#
 def Load_n_process_audio(file_path, separator_output_directory):
@@ -36,7 +54,7 @@ def Load_n_process_audio(file_path, separator_output_directory):
 
 
 #------------------------------------------------------------------------------------------------------------------------------#
-def predict(waveforms, language='persian', task='transcribe', sr=16000):
+def predict(waveforms, language='persian', task='transcribe', sr=16000, float_type=torch.float32):
     """
     Predict transcriptions and gender classification for the given waveforms.
 
@@ -58,7 +76,7 @@ def predict(waveforms, language='persian', task='transcribe', sr=16000):
         GNet_Input = GNet_Processor.pipe(wave)
         Whisper_Input = wave/max(wave) # for normalization
         Whisper_Input_Features = Whisper_Processor(Whisper_Input, sampling_rate=sr, return_tensors="pt").input_features
-        Whisper_Input_Features = Whisper_Input_Features.to(default_device)
+        Whisper_Input_Features = Whisper_Input_Features.to(float_type).to(default_device)
 
         Whisper_Predicted_ids = ASR_Whisper.generate(Whisper_Input_Features, forced_decoder_ids=forced_decoder_ids)
         Transcription = Whisper_Processor.batch_decode(Whisper_Predicted_ids, skip_special_tokens=True)
@@ -74,7 +92,7 @@ def predict(waveforms, language='persian', task='transcribe', sr=16000):
 
 
 #--------------------------------------------------------------------------------------------------------------------#
-def pipeline(file_path, separator_output_directory, whisper_language='persian', whisper_task='transcribe', sr=16000):
+def pipeline(file_path, separator_output_directory, whisper_language='persian', whisper_task='transcribe', sr=16000, float_type=torch.float32):
     """
     Main processing pipeline to handle audio input and generate transcriptions with gender classification.
 
@@ -93,7 +111,7 @@ def pipeline(file_path, separator_output_directory, whisper_language='persian', 
     Waveforms = Load_n_process_audio(file_path, separator_output_directory)
 
     # generate transcriptions with gender of the speaker
-    Results = predict(Waveforms, language=whisper_language, task=whisper_task, sr=16000)
+    Results = predict(Waveforms, language=whisper_language, task=whisper_task, sr=16000, float_type=float_type)
 
     # correct spell errors and normalize text
     for i, result in enumerate(Results):
@@ -101,3 +119,69 @@ def pipeline(file_path, separator_output_directory, whisper_language='persian', 
     
     return Results
 #--------------------------------------------------------------------------------------------------------------------#
+
+
+if __name__ == "__main__":
+
+    default_device = select_device()
+    
+    # Load Models
+    Gender_Classification_MLP = GNet.ModelLoader(GNet.ModelType.best_epoch, device=default_device)
+    ASR_Whisper = AutoModelForSpeechSeq2Seq.from_pretrained(pretrained_model_name_or_path="openai/whisper-large-v3", torch_dtype=torch.float16).to(default_device)
+
+    print('Models Loaded')
+
+    # Load Processors
+    GNet_Processor = GNet.WaveFormProcess()
+    Whisper_Processor = AutoProcessor.from_pretrained("openai/whisper-large-v3")
+
+    print('Processors Loaded')
+
+    
+    # Create API instance
+    app = FastAPI()
+
+    @app.get("/", response_class=HTMLResponse)
+    async def root():
+        html_content = """
+        <html>
+            <head>
+                <title>API Home</title>
+            </head>
+            <body>
+                <h1>Welcome to the API</h1>
+                <p>Access the API documentation <a href="/docs">here</a>.</p>
+            </body>
+        </html>
+        """
+        return html_content
+
+    # Define the response model
+    class TranscriptionResponse(BaseModel):
+        transcription: str
+        gender: str
+
+    # The directory where uploaded audio files will be saved
+    AUDIO_UPLOAD_DIRECTORY = "./uploaded_audio"
+
+    # Ensure the directories exist
+    os.makedirs(AUDIO_UPLOAD_DIRECTORY, exist_ok=True)
+
+    @app.post("/upload_and_process_audio/")
+    async def upload_and_process_audio(file: UploadFile = File(...)):
+        # Save the uploaded file with its original filename and format
+        saved_file_path = os.path.join(AUDIO_UPLOAD_DIRECTORY, file.filename)
+
+        with open(saved_file_path, "wb") as buffer:
+            shutil.copyfileobj(file.file, buffer)
+
+        # Use the pipeline to process the saved audio file
+        results = pipeline(saved_file_path, AUDIO_UPLOAD_DIRECTORY, float_type=torch.float16)
+
+        # Format the response (transcriptions and gender classifications)
+        response = [{"transcription": item[0], "gender": item[1]} for item in results]
+
+        return {"results": response}
+    
+
+    uvicorn.run(app, host="127.0.0.1", port=8000, timeout_keep_alive=900, workers=4)
